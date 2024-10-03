5f536be29f5eeefecbc849f846eef4e37dd233ab
manana <manana@A6E32>
2024-10-03 11:02:30 +0200

    First commit

---
+++ src/main/java/com/core/timmy/TimmyApplication.java
@@ -0,0 +1,13 @@
+package com.core.timmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/timmy/TimmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.timmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
