db49d08f57b76af43f5a6e97f18448b3d4f76357
rodal90 <alessmenes@gmail.com>
2025-01-20 10:17:18 +0100

    SCrypt workingnow commit

--- src/test/java/com/core/timmy/TimmyApplicationTests.java
+++ src/test/java/com/core/timmy/TimmyApplicationTests.java
@@ -3,11 +3,14 @@ package com.core.timmy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest               //activa el unit testing
 class TimmyApplicationTests {
 
-	@Test
+	@Test            //cada uno que sea un metodo para hacer test tendra la anotación test. 
+	
 	void contextLoads() {
+		
+		
 	}
 
 }
