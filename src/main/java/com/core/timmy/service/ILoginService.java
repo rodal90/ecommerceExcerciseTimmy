d8ce69dbc6b83a5ba96e6d743666e9efdc64079c
rodal90 <alessmenes@gmail.com>
2025-01-30 09:51:00 +0100

    minor changes 2 commit

--- src/main/java/com/core/timmy/service/ILoginService.java
+++ src/main/java/com/core/timmy/service/ILoginService.java
@@ -1,12 +1,10 @@
 package com.core.timmy.service;
 //tampoco tiene repositorio ni modelo
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.stereotype.Service;
 
 import com.core.timmy.data.model.Login;
-import com.core.timmy.data.model.User;
-import java.util.Optional;
-import java.util.Set;
+
 
 @Service
 public interface ILoginService{
